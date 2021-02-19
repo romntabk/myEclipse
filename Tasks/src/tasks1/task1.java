b1c623ad6ef1d5b1353317472e17176685095a0a
romntabk <romntabk@mail.ru>
2021-02-19 10:15:52 +0300

    task1 started

---
+++ Tasks/src/tasks1/task1.java
@@ -0,0 +1,14 @@
+package tasks1;
+
+public class task1 {
+	
+	public static int minutesToSeconds(int minutes) {
+		return minutes*60;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
