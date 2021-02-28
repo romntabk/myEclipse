b7ce3b2927c6aaceb459255fd297af88da637d9e
romntabk <romntabk@mail.ru>
2021-02-28 13:33:30 +0300

    yes, i seen mandelbrot

--- FourthLab/src/mypack/FractalExplorer.java
+++ FourthLab/src/mypack/FractalExplorer.java
@@ -1,6 +1,11 @@
 package mypack;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.geom.Rectangle2D;
 
 import javax.swing.JButton;
@@ -23,34 +28,49 @@ public class FractalExplorer{
 		JFrame jFrame = new JFrame("Fractals");
 		jFrame.add(jImageDisplay,BorderLayout.CENTER);
 		JButton buttonReset = new JButton("Reset");
+		buttonReset.addActionListener(new buttonResetHandler());
+		buttonReset.setVisible(true);
+		jFrame.add(buttonReset,BorderLayout.NORTH);
+		jImageDisplay.addMouseListener(new MouseHandler());	
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.pack();
         jFrame.setVisible(true);
         jFrame.setResizable(false);
 	}
-	private void drawFractal()
+	public void drawFractal()
 	{
-		for (int x=0; x<size; x++){
-			for (int y=0; y<size; y++){        
-				double xCoord = fractalGenerator.getCoord(complexRange.x, complexRange.x + complexRange.width, size, x);
-				double yCoord = fractalGenerator.getCoord(complexRange.y, complexRange.y + complexRange.height, size, y);
-				int iteration = fractal.numIterations(xCoord, yCoord);    
-				if (iteration == -1){
-					display.drawPixel(x, y, 0);
-				}    
-				else {
-					float hue = 0.7f + (float) iteration / 200f;
-					int rgbColor = Color.HSBtoRGB(hue, 1f, 1f);
-	                
-	                    /** Update the display with the color for each pixel. */
-	                    display.drawPixel(x, y, rgbColor);
-	                }
-	                
-	            }
-	        }
-	      
-	        jImageDisplay.repaint();
-	    }
-
+		for (int x=0; x<size; ++x){
+			for (int y=0; y<size; ++y){        
+				double xCoord = FractalGenerator.getCoord(complexRange.x, complexRange.x + complexRange.width, size, x);
+				double yCoord = FractalGenerator.getCoord(complexRange.y, complexRange.y + complexRange.height, size, y);  	
+				//System.out.println(xCoord+ " " +yCoord);
+				int iter = fractalGenerator.numIterations(xCoord, yCoord);
+				if (iter < 0) {
+					jImageDisplay.drawPixel(x, y, 0);
+					continue;
+				}
+				float hue = 0.7f + (float) iter / 200f;
+				int rgbColor = Color.HSBtoRGB(hue, 1f, 1f);
+				jImageDisplay.drawPixel(x, y, rgbColor);
+			}
+		}
+		jImageDisplay.repaint();
+	}
+	class buttonResetHandler implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			fractalGenerator.getInitialRange(complexRange);
+			drawFractal();
+		}	
+	}
+	class MouseHandler extends MouseAdapter{
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			double x = FractalGenerator.getCoord(complexRange.x, complexRange.x+complexRange.width, size, e.getX());
+			double y = FractalGenerator.getCoord(complexRange.y, complexRange.y+complexRange.height, size, e.getY());
+			fractalGenerator.recenterAndZoomRange(complexRange, x, y, 0.5);
+			drawFractal();
+		}
+	}
 
 }
--- FourthLab/src/mypack/Mandelbrot.java
+++ FourthLab/src/mypack/Mandelbrot.java
@@ -4,7 +4,7 @@ import java.awt.geom.Rectangle2D;
 import java.awt.geom.Rectangle2D.Double;
 
 public class Mandelbrot extends FractalGenerator {
-	public static final int MAX_ITERATIONS = 2000;
+	public static final int MAX_ITERATIONS = 10000;
 
 	@Override
 	public void getInitialRange(Rectangle2D.Double neponyal) {
@@ -15,17 +15,22 @@ public class Mandelbrot extends FractalGenerator {
 	}
 	@Override
 	public int numIterations(double x, double y) {
+		// 0 0 
+		// 0.995  -0.6
+		// 
 		int countIterations=0;
 		ComplexNumber complexNumber= new ComplexNumber(0,0);
 		ComplexNumber complexNumberBefore;
 		ComplexNumber complexNumberConst= new ComplexNumber(x,y);
+		//System.out.print(countIterations);	
 		while(complexNumber.lenPow2()<4) {
-			if(countIterations==MAX_ITERATIONS)
+			if(countIterations++==MAX_ITERATIONS)
 				return -1;
 			complexNumberBefore=(complexNumber.multiply(complexNumber)).sum(complexNumberConst);
-			
+
+			complexNumber=complexNumberBefore.sum(new ComplexNumber(0,0));
 		}
-		return 0;
+		return countIterations;
 	}
 	
 }
